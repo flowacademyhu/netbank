f0a9cc1acbc7d6a154af080368acf83dabef9f2a
Robert Boros <robbie13.boros@gmail.com>
2021-06-04 02:27:22 +0200

    add helpers

---
+++ src/test/java/hu/flowacademy/netbank/controller/helpers/LoanHelper.java
@@ -0,0 +1,31 @@
+package hu.flowacademy.netbank.controller.helpers;
+
+import hu.flowacademy.netbank.dto.LoanRequestDTO;
+import hu.flowacademy.netbank.model.Currency;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+import java.math.BigDecimal;
+
+import static io.restassured.RestAssured.*;
+
+public class LoanHelper {
+
+    public static final String API_LOANS = "/api/loans";
+
+    public static void requestLoan(String accountId, Currency currency, BigDecimal amount) {
+        given()
+            .log().all()
+            .body(LoanRequestDTO.builder()
+                    .accountId(accountId)
+                    .amount(amount)
+                    .build())
+            .contentType(ContentType.JSON)
+        .when()
+            .patch(API_LOANS+"/"+currency)
+        .then()
+            .log().all()
+            .statusCode(204);
+    }
+
+}
---
+++ src/test/java/hu/flowacademy/netbank/controller/helpers/TransactionHelper.java
@@ -0,0 +1,76 @@
+package hu.flowacademy.netbank.controller.helpers;
+
+import com.github.javafaker.Faker;
+import hu.flowacademy.netbank.dto.TransactionCreateDTO;
+import hu.flowacademy.netbank.model.Account;
+import hu.flowacademy.netbank.model.Transaction;
+import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.http.ContentType;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+
+public class TransactionHelper {
+
+    public static final String API_TRANSACTIONS = "/api/transactions";
+
+    private static Faker faker = new Faker();
+
+    public static TransactionCreateDTO create(String sender, String receiver) {
+        TransactionCreateDTO transaction = TransactionCreateDTO.builder()
+                .amount(BigDecimal.valueOf(faker.number().numberBetween(100, 10000)))
+                .senderAccountId(sender)
+                .receiverAccountId(receiver)
+                .build();
+
+        given()
+                .log().all()
+                .body(transaction)
+                .contentType(ContentType.JSON)
+        .when()
+                .post(API_TRANSACTIONS)
+        .then()
+                .log().all()
+                .statusCode(204);
+
+        return transaction;
+    }
+
+    public static void revert(String id) {
+        given()
+                .log().all()
+                .contentType(ContentType.JSON)
+        .when()
+                .post(API_TRANSACTIONS+"/"+id+"/revert")
+        .then()
+                .log().all()
+                .statusCode(204);
+    }
+
+    public static List<Transaction> findAll() {
+        return given()
+                .log().all()
+                .contentType(ContentType.JSON)
+        .when()
+                .post(API_TRANSACTIONS)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .extract().body().as(new TypeRef<>() {});
+    }
+
+    public static Transaction findOne(String id) {
+        return given()
+                .log().all()
+                .contentType(ContentType.JSON)
+        .when()
+                .post(API_TRANSACTIONS+"/"+id)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .extract().body().as(Transaction.class);
+    }
+}
