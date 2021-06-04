d1eafe7822c3891dcd6f177ddc77ad4013ecb1ba
Robert Boros <robbie13.boros@gmail.com>
2021-06-04 11:23:54 +0200

    fix cors

--- src/main/java/hu/flowacademy/netbank/config/WebConfig.java
+++ src/main/java/hu/flowacademy/netbank/config/WebConfig.java
@@ -13,7 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
         .allowedHeaders("*")
-        .allowCredentials(true)
         .allowedMethods("*")
         .allowedOrigins("*");
     }
